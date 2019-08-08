888d826dbd26af12fca5fbcbfdb320471087034b
Adam Cooper <adamcooperrc@gmail.com>
2019-08-08 11:33:43 -0400

    Fix gdb build script to be arch specific

--- build-gdb-with-python2.bash
+++ build-gdb-with-python2.bash
@@ -7,5 +7,5 @@ sudo apt build-dep gdb
 apt source gdb
 cd $(ls | grep gdb-)
 ./configure --with-python=$(which python2.7)
-dpkg-buildpackage -us -uc -nc
+dpkg-buildpackage -us -uc -nc -B
 sudo dpkg -i ../*.deb
