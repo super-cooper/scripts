d20156e53275e685b934478d8100f94961a90d65
Adam Cooper <adamcooperrc@gmail.com>
2019-08-08 12:16:48 -0400

    Add fix broken packages at end of script

--- build-gdb-with-python2.bash
+++ build-gdb-with-python2.bash
@@ -10,3 +10,4 @@ cd $(ls | grep gdb-)
 sed -Ei 's/(.*)--with-python=python3/\1--with-python=python2.7/g' debian/rules
 dpkg-buildpackage -us -uc -nc -B
 sudo dpkg -i ../*.deb
+sudo apt install --fix-broken
