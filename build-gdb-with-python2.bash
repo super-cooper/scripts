93329440bfde1d63a3ccccff2abcb742442c360a
Adam Cooper <adamcooperrc@gmail.com>
2019-08-08 11:17:53 -0400

    Add script for building gdb with python2.7

---
+++ build-gdb-with-python2.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd ~/Downloads
+
+rm -rf gdb && mkdir gdb && cd gdb
+sudo apt build-dep gdb
+apt source gdb
+cd $(ls | grep gdb-)
+./configure --with-python=$(which python2.7)
+dpkg-buildpackage -us -uc -nc
+sudo dpkg -i ../*.deb
