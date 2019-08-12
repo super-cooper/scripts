462e13cd023ef340c82660865d49494655bd347a
Adam Cooper <adamcooperrc@gmail.com>
2019-08-12 10:29:04 -0400

    Fix permissions in makeadmin script

--- mongo/makeadmin.js
+++ mongo/makeadmin.js
@@ -2,7 +2,5 @@ let db = new Mongo().getDB('admin');
 db.createUser({
     user: 'admin', 
     pwd: 'password', 
-    roles: [
-        {role: "userAdminAnyDatabase", db: "admin"}
-    ]
+    roles: ["userAdminAnyDatabase", "dbAdminAnyDatabase", "readWriteAnyDatabase", { role: "root", db: "admin" }]
 });
