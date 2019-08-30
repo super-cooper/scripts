3630047c8237dfa568a9470a7590d16e5e013fae
Adam Cooper <adamcooperrc@gmail.com>
2019-08-30 16:01:18 -0400

    Robustify mongo workflow scripts

--- cr.bash
+++ cr.bash
@@ -8,6 +8,7 @@ browser=vivaldi                                             # command to open yo
 
 # gets the name of the current checked-out branch
 branch=$(git branch | grep "\*" | cut -d ' ' -f2)
+upstream=$(git rev-parse --abbrev-ref "$branch@{upstream}" | sed 's/.*\///')
 if [ "$branch" == "master" ]; then
     echo "I won't put notes on master!!"
     exit 1
@@ -19,10 +20,10 @@ id=$(git config branch.$branch.note 2> /dev/null | xargs echo)
 # determines appropriate args based on whether an ID was found in git-notes
 if [ -z "$id" ]; then
     echo -e "\033[1;92mCreating NEW review...\033[0;0m"
-    other_args=( --rev origin/master... -y --check-clang-format --check-eslint --title $branch --email $email $@ )
+    other_args=( --rev origin/$upstream... -y --check-clang-format --check-eslint --title $branch --email $email $@ )
 else
     echo -e "\033[1;34mUpdating review...\033[0;0m"
-    other_args=( -i "$id" --rev origin/master... -y --check-clang-format --check-eslint --title $branch $@ )
+    other_args=( -i "$id" --rev origin/$upstream... -y --check-clang-format --check-eslint --title $branch $@ )
 fi
 
 # submits the CR
@@ -40,7 +41,7 @@ url=$(echo "$urlline" | sed -E 's/.+(http.+)/\1/g')
 id=$(echo "$url" | sed -E 's/.+\/([0-9]+).*/\1/g')
 
 # stores the ID in the branch's git-notes
-git config branch.$branch.note $id &> /dev/null
+git config branch.$branch.note $id
 
 # open the CR in the browser
 $browser "$url" &> /dev/null
--- eg.bash
+++ eg.bash
@@ -1,17 +1,18 @@
 branch=$(git branch | grep "\*" | cut -d ' ' -f2)
-id=$(/home/adam/dev/scripts/evergreen patch -d "$(git current-branch)" -y | grep ID | sed -E 's/.+ ([0-9]+)/\1/g')
+upstream=$(git rev-parse --abbrev-ref "$branch@{upstream}" | sed 's/.*\///')
+id=$(/home/adam/dev/scripts/evergreen patch -d "$(git current-branch)" -p mongodb-mongo-$upstream -y | grep ID | sed -E 's/.+ ([0-9]+)/\1/g')
+
+git fetch
+
 cd /home/adam/mongodb/mongo/src/mongo/db/modules/enterprise/ || exit
-git pull --rebase origin master
-git checkout master
-git pull --rebase origin master
-git checkout -
+git pull --rebase origin $upstream
+
 cd /home/adam/mongodb/mongo || exit
-git pull --rebase origin master
-git checkout master
-git pull --rebase origin master
-git checkout -
+git pull --rebase origin $upstream
+
 cd - || exit
-if [ -n "$(git diff $branch $(git merge-base $branch master))" ]; then
+if [ -n "$(git diff $branch $(git merge-base $branch origin/$upstream))" ]; then
   /home/adam/dev/scripts/evergreen patch-set-module -i "$id" -m enterprise -y
 fi
+
 vivaldi "https://evergreen.mongodb.com/patch/$id"
