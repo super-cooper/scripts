38cd24147a4508567d9030645820b45a6472de0d
Adam Cooper <adamcooperrc@gmail.com>
2019-09-25 15:45:07 -0400

    Change use of $HOME

--- mongo/eg.bash
+++ mongo/eg.bash
@@ -4,7 +4,7 @@ branch=$(git branch | grep "\*" | cut -d ' ' -f2)
 upstream=$(git rev-parse --abbrev-ref "$branch@{upstream}" | sed 's/.*\///')
 id=$(evergreen patch -d "$(git current-branch)" -p mongodb-mongo-$upstream -y | grep ID | sed -E 's/.+ ([0-9]+)/\1/g')
 
-if [ -n "$(git --git-dir=$HOME/mongodb/enterprise/.git cherry -v)" ]; then
+if [ -n "$(git --git-dir=/home/adam/mongodb/enterprise/.git cherry -v)" ]; then
   cd $HOME/mongodb/enterprise && evergreen patch-set-module -i "$id" -m enterprise -y
 fi
 
