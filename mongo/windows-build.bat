f2ff50b46115e0e808d5e005042f08ae33dde846
Adam Cooper <adam@adamcooper.sh>
2021-01-03 05:47:27 -0500

    Remove ninja flag

--- mongo/windows-build.bat
+++ mongo/windows-build.bat
@@ -7,4 +7,4 @@ venv\Scripts\activate
 python -m pip install -r etc\dev-requirements.txt
 python -m pip install -r etc\evgtest-requirements.txt
 
-python buildscripts\scons.py --dbg=on --ssl --link-model=object  CPPPATH="c:/sasl/include c:/snmp/include" LIBPATH="c:/sasl/lib c:/snmp/lib"  CPPDEFINES="_SILENCE_CXX17_ITERATOR_BASE_CLASS_DEPRECATION_WARNING MONGO_USE_GPERFTOOLS_TCMALLOC" MONGO_GIT_HASH="unknown" MONGO_VERSION="0.0.0" --install-mode=hygienic --ninja install-all
+python buildscripts\scons.py --dbg=on --ssl --link-model=object  CPPPATH="c:/sasl/include c:/snmp/include" LIBPATH="c:/sasl/lib c:/snmp/lib"  CPPDEFINES="_SILENCE_CXX17_ITERATOR_BASE_CLASS_DEPRECATION_WARNING MONGO_USE_GPERFTOOLS_TCMALLOC" MONGO_GIT_HASH="unknown" MONGO_VERSION="0.0.0" --install-mode=hygienic install-all
